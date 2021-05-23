78ff27e5e39cbb78044bb7957f2a8fd396acd4b7
Laura Orozco <69904799+Melilaura@users.noreply.github.com>
2021-05-22 20:30:27 -0500

    git final zip

--- InteractiveStory/src/controller/Controller.java
+++ InteractiveStory/src/controller/Controller.java
@@ -1,5 +1,26 @@
 package controller;
 
-public class Controller {
+import model.Scene;
+import view.Main;
+import processing.core.PApplet;
 
+public class Controller {
+	
+	Scene scene;
+	boolean tipo;
+
+	public Controller(PApplet app) {
+		
+		scene = new Scene();
+		tipo = true;
+		
+	}
+	
+	public void Words(PApplet app) {
+		
+		
+		scene.setup(app);
+		
+	}
+	
 }
